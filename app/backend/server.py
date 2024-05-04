03fefacdb5a0a096fb58cf2d7161133580e95780
Troy Dutton <troydutton@utexas.edu>
2024-05-04 06:41:23 -0500

    Move inference to frontend

--- app/backend/server.py
+++ app/backend/server.py
@@ -1,3 +1,4 @@
+import inference
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from PIL import Image
@@ -15,8 +16,10 @@ def image():
 
     image.show()
 
+    result = inference.infer(image)
+
     return jsonify({"message": "Image uploaded successfully"}), 200
 
 
 if __name__ == "__main__":
-    app.run(host="100.110.148.60", port=5000, debug=True)
+    app.run(host="100.110.148.13", port=5000, debug=True)
